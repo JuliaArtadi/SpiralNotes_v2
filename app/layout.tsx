af73e8a4c0a5a1a5150ed842a3472f4dd6975410
Julia Bandera <julia.a.bandera@gmail.com>
2024-03-08 19:38:03 +0100

    deploy fixes

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import "@/app/ui/global.css";
+import SideNav from "@/app/ui/dashboard/sidenav";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Spiral Notes",
+    default: "Spiral Notes",
+  },
+  description: "Twój księżycowy notatnik",
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <></>;
+}
